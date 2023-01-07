8b7f25e6da58140befbf5c830a23e4372eec99b4
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2023-01-07 21:59:20 +0530

    github

--- src/component/Github/GitHub.jsx
+++ src/component/Github/GitHub.jsx
@@ -5,32 +5,24 @@ import "./GitHub.css";
 
 
 const GitHub = () => {
-  const selectLastHalfYear = contributions => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 8;
-
-    return contributions.filter(day => {
-      const date = new Date(day.date);
-      const monthOfDay = date.getMonth();
-
-      return (
-        date.getFullYear() === currentYear &&
-        monthOfDay > currentMonth - shownMonths &&
-        monthOfDay <= currentMonth
-      );
-    });
-  };
+ 
 
   return (
     <div>
       <h2 className="git" data-aos="fade-right">My GitHub Calender</h2>
       <div className='github_Calender' data-aos="fade-right">
-        <GitHubCalendar style={{margin:"auto"}}
+        {/* <GitHubCalendar style={{margin:"auto"}}
           username='kundan799'
-          transformData={selectLastHalfYear}
-          hideColorLegend
+          
+        /> */}
+          <GitHubCalendar
+          username="kundan799"
+          blockSize={15}
+          blockMargin={5}
+         color={ "green"}
+          fontSize={12}
         />
+
       </div>
     </div>
 
